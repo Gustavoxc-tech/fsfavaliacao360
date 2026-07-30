c029b99fea056f537a4cb68b437c507f85e03c15
Gustavoxc-tech <xaviergustavo50.3@gmail.com>
2026-07-29 21:21:16 -0300

    Update auth.tsx

--- src/routes/auth.tsx
+++ src/routes/auth.tsx
@@ -11,7 +11,7 @@ import { toast } from "sonner";
 
 export const Route = createFileRoute("/auth")({
   ssr: false,
-  head: () => ({ meta: [{ title: "Entrar — PeopleZenith" }] }),
+  head: () => ({ meta: [{ title: "Entrar — EVSHIFT" }] }),
   component: AuthPage,
 });
 
